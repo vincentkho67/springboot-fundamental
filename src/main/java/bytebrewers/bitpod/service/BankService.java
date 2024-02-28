ed63842da3c6f2e0d6c27675601eaa0b39bd6a88
rizky <rizkyrahman1095@gmail.com>
2024-02-28 18:16:34 +0700

    [FEATURE] add feature crud bank

--- src/main/java/bytebrewers/bitpod/service/BankService.java
+++ src/main/java/bytebrewers/bitpod/service/BankService.java
@@ -1,4 +1,18 @@
 package bytebrewers.bitpod.service;
 
+import bytebrewers.bitpod.utils.dto.request.bank.BankSearchDTO;
+import org.springframework.http.ResponseEntity;
+
 public interface BankService {
+    ResponseEntity<?> createBank(BankSearchDTO bankSearchDTO);
+
+    ResponseEntity<?>  getById(Integer id);
+
+    ResponseEntity<?> update(Integer id, BankSearchDTO bankSearchDTO);
+
+    ResponseEntity<?> deleteById(Integer id);
+
+    ResponseEntity<?> getCustomerPerPage(Integer page, Integer size, String sortBy, String direction, BankSearchDTO bankSearchDTO);
+
+
 }
