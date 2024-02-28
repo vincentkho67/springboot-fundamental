044e2f1259fbeb6f2e70142b80b95e9630efb89c
Vincent <vincentkho67@gmail.com>
2024-02-28 11:16:33 +0700

    [FEATURE] init project

---
+++ src/main/java/bytebrewers/bitpod/BitPodApplication.java
@@ -0,0 +1,13 @@
+package bytebrewers.bitpod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitPodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitPodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bytebrewers/bitpod/BitPodApplicationTests.java
@@ -0,0 +1,13 @@
+package bytebrewers.bitpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitPodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
