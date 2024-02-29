f3876893accd1d083ac98d626069c34ed37f0bea
Vincent <vincentkho67@gmail.com>
2024-02-29 08:58:17 +0700

    [IMPROVE] basic skeleton of CRUD done

--- src/main/java/bytebrewers/bitpod/service/TransactionService.java
+++ src/main/java/bytebrewers/bitpod/service/TransactionService.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 public interface TransactionService {
     Page<Transaction> getAll(Pageable pageable, TransactionDTO req);
     Transaction create(TransactionDTO req);
-    Transaction update(String id, TransactionDTO req);
     Transaction getById(String id);
     void delete(String id);
 }
--- src/main/java/bytebrewers/bitpod/service/impl/TransactionServiceImpl.java
+++ src/main/java/bytebrewers/bitpod/service/impl/TransactionServiceImpl.java
@@ -1,20 +1,32 @@
 package bytebrewers.bitpod.service.impl;
 
+import bytebrewers.bitpod.entity.Bank;
+import bytebrewers.bitpod.entity.Portfolio;
+import bytebrewers.bitpod.entity.Stock;
 import bytebrewers.bitpod.entity.Transaction;
 import bytebrewers.bitpod.repository.TransactionRepository;
+import bytebrewers.bitpod.service.BankService;
+import bytebrewers.bitpod.service.PortfolioService;
+import bytebrewers.bitpod.service.StockService;
 import bytebrewers.bitpod.service.TransactionService;
 import bytebrewers.bitpod.utils.dto.request.transaction.TransactionDTO;
 import bytebrewers.bitpod.utils.specification.GeneralSpecification;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
 public class TransactionServiceImpl implements TransactionService {
     private final TransactionRepository transactionRepository;
+    private final StockService stockService;
+    private final PortfolioService portfolioService;
+    private final BankService bankService;
 
 
     @Override
@@ -23,23 +35,24 @@ public class TransactionServiceImpl implements TransactionService {
         return transactionRepository.findAll(specification, pageable);
     }
 
+    @Transactional
     @Override
     public Transaction create(TransactionDTO req) {
-        return null;
-    }
-
-    @Override
-    public Transaction update(String id, TransactionDTO req) {
-        return null;
+        Stock stock = stockService.getById(req.getStockId());
+        Portfolio portfolio = portfolioService.getById(req.getPortfolioId());
+        Bank bank = bankService.getById(req.getBankId());
+        Transaction newTransaction = req.toEntity(stock, portfolio, bank);
+        return transactionRepository.save(newTransaction);
     }
 
     @Override
     public Transaction getById(String id) {
-        return null;
+        return transactionRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Transaction not found"));
     }
 
     @Override
     public void delete(String id) {
-
+        transactionRepository.deleteById(id);
     }
 }
