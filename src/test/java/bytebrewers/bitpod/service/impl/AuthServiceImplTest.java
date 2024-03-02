3babc21e914040365d60e367b313f75727f32fe7
Vincent <vincentkho67@gmail.com>
2024-03-02 17:28:37 +0700

    [IMPROVE] Service layer unit test implementation

---
+++ src/test/java/bytebrewers/bitpod/service/impl/AuthServiceImplTest.java
@@ -0,0 +1,24 @@
+package bytebrewers.bitpod.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceImplTest {
+
+    @Test
+    void initSuperAdmin() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void registerAdmin() {
+    }
+}
---
+++ src/test/java/bytebrewers/bitpod/service/impl/RoleServiceImplTest.java
@@ -0,0 +1,58 @@
+package bytebrewers.bitpod.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import bytebrewers.bitpod.entity.Role;
+import bytebrewers.bitpod.repository.RoleRepository;
+import bytebrewers.bitpod.utils.enums.ERole;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class RoleServiceImplTest {
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @InjectMocks
+    private RoleServiceImpl roleService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testGetOrSaveRoleExists() {
+        ERole expectedRole = ERole.ROLE_MEMBER;
+        Role existingRole = Role.builder().role(expectedRole).build();
+
+        when(roleRepository.findByRole(expectedRole)).thenReturn(Optional.of(existingRole));
+        Role resultRole = roleService.getOrSave(expectedRole);
+
+        assertEquals(existingRole, resultRole);
+        verify(roleRepository, times(1)).findByRole(expectedRole);
+        verify(roleRepository, never()).save(any(Role.class));
+    }
+
+    @Test
+    void testGetOrSaveRoleNotExists() {
+        ERole expectedRole = ERole.ROLE_ADMIN;
+        Role existRole = null;
+        if (roleRepository.findByRole(expectedRole).isEmpty()) {
+            existRole = Role.builder().role(expectedRole).build();
+            roleRepository.save(existRole);
+        }
+        Role resultRole = existRole;
+
+        assertNotNull(resultRole);
+        assertEquals(expectedRole, resultRole.getRole());
+        verify(roleRepository, times(1)).findByRole(expectedRole);
+        verify(roleRepository, times(1)).save(any(Role.class));
+    }
+}
