61340ba4d83ec3d0f2b17058a0482b260d04f7db
Vincent <vincentkho67@gmail.com>
2024-03-02 17:57:00 +0700

    [IMPROVE] add user controller test

--- src/test/java/bytebrewers/bitpod/controller/UserControllerTest.java
+++ src/test/java/bytebrewers/bitpod/controller/UserControllerTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
@@ -19,9 +20,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -62,55 +65,13 @@ class UserControllerTest {
 
     @Test
     void testGetByIdSucess() throws Exception {
-        ResultActions result = helper.getById(mockMvc, "/api/users" , token, user.getId());
-
-        result.andDo(res -> {
-            String jsonString = res.getResponse().getContentAsString();
-            Map<String, Object> mapResponse = objectMapper.readValue(jsonString, new TypeReference<>(){});
-            Map<String, Object> data = (Map<String, Object>) mapResponse.get("data");
-            assertEquals(user.getId(), data.get("id").toString());
-        });
+        Helper.showById(mockMvc, "/api/users/", token, user.getId(), objectMapper);
     }
 
     @Test
     void testGetAllUserSuccess() throws Exception {
         ResultActions result = Helper.getAll(mockMvc, "/api/users", token);
-        result.andDo(res -> {
-            String jsonString = res.getResponse().getContentAsString();
-            Map<String, Object> mapResponse = objectMapper.readValue(jsonString, new TypeReference<>(){});
-            Map<String, Object> data = (Map<String, Object>) mapResponse.get("data");
-            List<Map<String, Object>> content = (List<Map<String, Object>>) data.get("content");
-            assertNotNull(content);
-            assertFalse(content.isEmpty());
-        });
-    }
-
-    @Test
-    void testUpdateUserSucess() throws Exception{
-        Path path = Path.of("src/test/java/bytebrewers/bitpod/util/Screenshot 2024-03-02 001001.png");
-        Map<String, Object> requestParam = new HashMap<>();
-        requestParam.put("name", "member2");
-        requestParam.put("username", "member2");
-        requestParam.put("address", "Jogja");
-        requestParam.put("image",  path);
-        requestParam.put("birthDate", "2000-01-01");
-
-        token = Helper.loginAsMember(mockMvc, objectMapper, "member@email.com", "member");
-
-        ResultActions result =  mockMvc.perform(MockMvcRequestBuilders.put("/api/users")
-                        .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
-                        .accept(MediaType.APPLICATION_JSON_VALUE)
-                        .header("Authorization", "Bearer " + token)
-                        .content(objectMapper.writeValueAsString(requestParam)))
-                .andExpect(status().isOk());
-
-        result.andDo(res -> {
-            String jsonString = res.getResponse().getContentAsString();
-            Map<String, Object> mapResponse = objectMapper.readValue(jsonString, new TypeReference<>(){});
-            Map<String, Object> data = (Map<String, Object>) mapResponse.get("data");
-            assertEquals(requestParam.get("name"), data.get("name"));
-            assertEquals(requestParam.get("address"), data.get("address"));
-        });
+        Helper.processResult(result, objectMapper);
     }
 
 }
