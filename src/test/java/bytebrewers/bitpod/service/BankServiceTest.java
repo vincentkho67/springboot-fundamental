149ca9ed69e77f292ef77c8e83f1f7649609337c
Vincent <vincentkho67@gmail.com>
2024-03-03 12:49:09 +0700

    [IMPROVE] we just need to implement the interface test

---
+++ src/test/java/bytebrewers/bitpod/service/BankServiceTest.java
@@ -0,0 +1,56 @@
+package bytebrewers.bitpod.service;
+
+import bytebrewers.bitpod.entity.Bank;
+import bytebrewers.bitpod.repository.BankRepository;
+import bytebrewers.bitpod.service.impl.BankServiceImpl;
+import bytebrewers.bitpod.utils.dto.request.bank.BankDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class BankServiceTest {
+    @Mock
+    private BankRepository bankRepository;
+
+    @InjectMocks
+    private BankServiceImpl bankService;
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void create() {
+        Bank bank = new Bank();
+
+        BankDTO bankDTO = BankDTO.builder()
+                .name("Bank of America")
+                .address("New York")
+                .build();
+
+        when(bankRepository.save(Mockito.any(Bank.class))).thenReturn(bank);
+
+        Bank savedBank = bankService.create(bankDTO);
+
+        assertNotNull(savedBank);
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
