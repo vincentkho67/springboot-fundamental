0c409d39ed6fd879e63df1d2a8a72616a1401d50
Muhammad Abid Sutisna <abidsutis@gmail.com>
2024-03-04 14:42:13 +0700

    [IMPROVE] improve validation

--- src/main/java/bytebrewers/bitpod/utils/dto/request/user/AuthRequest.java
+++ src/main/java/bytebrewers/bitpod/utils/dto/request/user/AuthRequest.java
@@ -2,6 +2,7 @@ package bytebrewers.bitpod.utils.dto.request.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,7 +16,7 @@ import lombok.Setter;
 @Builder
 public class AuthRequest {
 
-    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$", message = "Please provide a valid email address")
     @NotBlank(message = "Email cannot be empty")
     private String email;
 
--- src/main/java/bytebrewers/bitpod/utils/dto/request/user/RegisterDTO.java
+++ src/main/java/bytebrewers/bitpod/utils/dto/request/user/RegisterDTO.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,7 +17,7 @@ import lombok.Setter;
 @Builder
 public class RegisterDTO {
 
-    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$", message = "Please provide a valid email address")
     @NotBlank(message = "Email cannot be empty")
     private String email;
 
